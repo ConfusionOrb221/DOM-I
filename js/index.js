34c62378d316ffcf07cf1087a4b6b748253c8b96
ConfusionOrb221 <nate1898@gmail.com>
2019-10-29 23:00:09 -0400

    MVP push

--- js/index.js
+++ js/index.js
@@ -41,20 +41,29 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"]);
 
+//header
 let nav = document.querySelectorAll('a');
 let nav_info = Object.values(siteContent.nav);//converts nav into an array so i can get the value using the index allowing me to do it all in one for loop.
 for(i = 0; i < nav.length; i++){
-  nav[i].textContent = nav_info[i]; 
+  nav[i].textContent = nav_info[i];
+  nav[i].style.color = "green"; 
 }
 
+//main
 let main = document.querySelector(".cta-text").children;
 let main_info = Object.values(siteContent.cta);
 for(i = 0; i < main.length; i++){
-  main[i].textContent = main_info[i];
+  if(main_info[i] === siteContent.cta.h1){
+    main[i].innerHTML = "DOM <br /> Is <br /> Awesome";
+  }//this is to fix the text break seen in the origional but not done in the index
+  else{
+    main[i].textContent = main_info[i];
+  }
 }
 let main_logo = document.getElementById("cta-img");
 main_logo.setAttribute("src", siteContent.cta["img-src"]);
 
+//content/info
 let content = document.querySelectorAll(".main-content > div > div > h4, .main-content > div > div > p, .main-content > img");
 let info = Object.values(siteContent["main-content"]);
 for(i = 0; i < content.length; i++){
@@ -66,4 +75,39 @@ for(i = 0; i < content.length; i++){
   }
 }
 
+//contact
+let contact = document.querySelector(".contact").children;
+let contact_info = Object.values(siteContent.contact);
+for(i = 0; i < contact.length; i++){
+  if(contact_info[i] === siteContent.contact.address){
+    contact[i].innerHTML = "123 Way 456 Street <br /> Somewhere, USA"
+  } //this is to fix the text break seen in the origional but not done in the index
+  else{
+    contact[i].textContent = contact_info[i];
+  }
+}
+let testing = document.getElementsByClassName("main-content");
+console.log(testing);
+//footer
+document.querySelector("footer > p").textContent = siteContent.footer.copyright;
 
+//appending
+let navbar = document.querySelector("nav");
+
+let new_link = document.createElement('a');
+let text_node = document.createTextNode("Append");
+
+
+let new_link2 = document.createElement('a');
+let text_node2 = document.createTextNode("Prepend");
+
+new_link.style.color = "green";
+new_link2.style.color = "green";
+new_link.href = "#";
+new_link2.href ="#";
+
+new_link.appendChild(text_node);
+new_link2.appendChild(text_node2);
+
+navbar.appendChild(new_link);
+navbar.prepend(new_link2);
