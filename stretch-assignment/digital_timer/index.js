4ebd6d3e6bf80aaaea167d28f55869556bfbdccd
ConfusionOrb221 <nate1898@gmail.com>
2019-10-31 18:19:37 -0400

    finished stretch goals using a number array and converting an int into an array and using a map to assign values to the dom elements

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,46 +1,37 @@
 let timer = 0.00;
 let interval = null;
-let secondTens = document.getElementById("secondTens");
-let secondOnes = document.getElementById("secondOnes");
-let msHundreds = document.getElementById("msHundreds");
-let msTens = document.getElementById("msTens");
+let numbers = [
+    document.getElementById("msHundreds"),
+    document.getElementById("msTens"),
+    document.getElementById("secondOnes"),
+    document.getElementById("secondTens"),
+];
+numbers.forEach(i =>i.textContent = 0);
 let startButton = document.getElementById("start");
+let resetButton = document.getElementById("reset");
 
-
-secondTens.textContent = 0;
-secondOnes.textContent = 0;
-msHundreds.textContent = 0;
-msTens.textContent = 0;
 function update(){//when called upates the milisecond number by 1
-    timer += 0.01;
-    let arr = [...timer + ''].map((i) => { return Number(i); })
-    if(timer >= 10){
-        secondTens.textContent = arr[0];
-        secondOnes.textContent = arr[1];
-        msHundreds.textContent = arr[3];
-        msTens.textContent = arr[4];
-        msTens.parentElement.style.color = "red";
+    timer += 1;
+    let numberArr = [...timer + ''].reverse();
+    numberArr.forEach((i, index) => numbers[index].textContent = i);
+    if(timer === 1000){
+        numbers[0].parentElement.style.color = "red";
         clearInterval(interval);
         return;
     }
-    else{
-        secondOnes.textContent = arr[0];
-        msHundreds.textContent = arr[2];
-        msTens.textContent = arr[3];
-    }
-    
+}
+startButton.addEventListener("click", start);
+resetButton.addEventListener("click", reset);
+function start(){
+    startButton.disabled = true;
+    interval = setInterval(update, 10);
 }
 
-startButton.addEventListener("click", start);
-
-function start(){
+function reset(){
+    timer = 0;
     clearInterval(interval);
-    if(timer === 0.00){
-        interval = setInterval(update, 10);
-    }
-    else{
-        timer = 0.00;
-        msTens.parentElement.style.color = "black";
-        interval = setInterval(update, 10);
-    }
+    numbers.forEach(i => i.textContent = 0);
+    numbers[0].parentElement.style.color = "black";
+    startButton.disabled = false;
+
 }
